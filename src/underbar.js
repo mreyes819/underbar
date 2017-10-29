a96c05a87d8b6e4c59c7b44d23e18f66a1d40185
matt <mttrys@gmail.com>
2017-10-28 23:23:49 -0700

    reformat: contains

--- src/underbar.js
+++ src/underbar.js
@@ -172,9 +172,7 @@
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
     return _.reduce(collection, function(wasFound, item) {
-      if (wasFound) {
-        return true;
-      }
+      if (wasFound) return true;
       return item === target;
     }, false);
   };
